a98f7c0a4ca9a3656b245c52a096ec5876f803bd
janos0348 <katona.janos@szalezi.dual>
2022-11-14 09:10:59 +0100

    bejelentkezés gui

--- src/amoba/amobaGUI.java
+++ src/amoba/amobaGUI.java
@@ -15,8 +15,13 @@ public class amobaGUI extends javax.swing.JFrame {
      */
     public amobaGUI() {
         initComponents();
+        initComponents2();
     }
-
+        private void initComponents2(){
+        
+        
+        }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,21 +31,125 @@ public class amobaGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pinNumsJPanel = new javax.swing.JPanel();
+        pinSpaceJPanel = new javax.swing.JPanel();
+        keverJCheckBox = new javax.swing.JCheckBox();
+        kodJLabel = new javax.swing.JLabel();
+        passJTextField = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        pinNumsJPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(null));
+        pinNumsJPanel.setToolTipText("");
+
+        javax.swing.GroupLayout pinNumsJPanelLayout = new javax.swing.GroupLayout(pinNumsJPanel);
+        pinNumsJPanel.setLayout(pinNumsJPanelLayout);
+        pinNumsJPanelLayout.setHorizontalGroup(
+            pinNumsJPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 210, Short.MAX_VALUE)
+        );
+        pinNumsJPanelLayout.setVerticalGroup(
+            pinNumsJPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 155, Short.MAX_VALUE)
+        );
+
+        pinSpaceJPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(null));
+
+        keverJCheckBox.setText("kever");
+        keverJCheckBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                keverJCheckBoxActionPerformed(evt);
+            }
+        });
+
+        kodJLabel.setText("kód:");
+
+        javax.swing.GroupLayout pinSpaceJPanelLayout = new javax.swing.GroupLayout(pinSpaceJPanel);
+        pinSpaceJPanel.setLayout(pinSpaceJPanelLayout);
+        pinSpaceJPanelLayout.setHorizontalGroup(
+            pinSpaceJPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pinSpaceJPanelLayout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(pinSpaceJPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(kodJLabel)
+                    .addComponent(keverJCheckBox, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(passJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(48, Short.MAX_VALUE))
+        );
+        pinSpaceJPanelLayout.setVerticalGroup(
+            pinSpaceJPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pinSpaceJPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(keverJCheckBox)
+                .addGap(18, 18, 18)
+                .addComponent(kodJLabel)
+                .addGap(18, 18, 18)
+                .addComponent(passJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(55, Short.MAX_VALUE))
+        );
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null));
+        jPanel1.setToolTipText("");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        jMenu1.setText("Program");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Játék elrendezése");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(241, Short.MAX_VALUE)
+                .addComponent(pinNumsJPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(62, 62, 62)
+                .addComponent(pinSpaceJPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(260, 260, 260)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(54, 54, 54)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 184, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(pinNumsJPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(44, 44, 44))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(pinSpaceJPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(59, 59, 59))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void keverJCheckBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_keverJCheckBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_keverJCheckBoxActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +187,14 @@ public class amobaGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JCheckBox keverJCheckBox;
+    private javax.swing.JLabel kodJLabel;
+    private javax.swing.JTextField passJTextField;
+    private javax.swing.JPanel pinNumsJPanel;
+    private javax.swing.JPanel pinSpaceJPanel;
     // End of variables declaration//GEN-END:variables
 }
